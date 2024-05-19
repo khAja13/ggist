32b2d7711dffd205d0af84fc09e150c50da61b0f
khajashaik13 <khajashaik1000@gmail.com>
2024-05-19 21:14:02 +0530

    basic changes done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/metrics",
+        destination: "/api/metrics",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
